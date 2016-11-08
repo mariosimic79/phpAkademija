0e69001a1f958a4da567f3a0dfc566dd529c74f5
Mario <Mario.simic79@gmail.com>
2016-11-08 19:46:20 +0100

    Lekcija 6 - Laravel i Composer

---
+++ lesson-06/mvc-app/app/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * example config file
+ */
+return [
+    //'some_config_key' => 'some config value',
+
+    'url'           => 'http://localhost/phpAkademija/phpAkademija/lesson-06/mvc-app/',
+    'mode'          => 'development',
+
+    'admin_email'   => 'student@etf.hr',
+    'admin_password' => 'phpakademija',
+
+    'db_name'       => 'phpakademija',
+    'db_user'       => 'root',
+    'db_password'   => '',
+];
